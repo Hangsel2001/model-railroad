f67a28a3498fb0fd25070f6af4474055e104364b
Markus Hängsel <jag@markushangsel.com>
2017-05-19 00:04:45 +0200

    parsar lok-info efter minimala startbehov (ljus + fart/riktning)

--- command-node/spec/z21-udp-helper.spec.js
+++ command-node/spec/z21-udp-helper.spec.js
@@ -30,21 +30,35 @@
         const actual = helper.getXorPackage(input);
         expect(actual).toEqual(expected);
     });
-
-    xit("parses a loco package",()=>{
-        const package = null;
-        helper.parsePackage(package);
+/*
+<Buffer 0e 00 40 00 ef 00 03 04 47 10 00 00 00 bf>
+<Buffer 0e 00 40 00 ef 00 03 04 00 10 00 00 00 f8>
+<Buffer 0e 00 40 00 ef 00 03 04 80 10 00 00 00 78>
+<Buffer 0e 00 40 00 ef 00 03 04 ca 10 00 00 00 32>
+<Buffer 0e 00 length
+        40 00 X
+        ef Lokinfo
+        00 03 address
+        04 Busy + steps (128)
+        80 1 010000
+        10 00 00 00 78>
+POWER ON  <Buffer 07 00 40 00 61 01 60 06 00 a1 00 83 7c>
+POWER OFF <Buffer 07 00 40 00 61 00 61 06 00 a1 00 82 7d>
+*/
+    it("parses a loco package",()=>{
+        const package = Buffer.from([0x0e,0x00,0x40,0x00,0xef,0x00,0x03,0x04,0xca,0x10,0x00,0x00,0x00,0x78]);
+        const actual = helper.parsePackage(package);
         const expected = {
             type: "loco",
             address: 3,
             direction: "forward",
-            speed: 50,
-            speedSteps: 120,
+            speed: 74,
+            speedSteps: 128,
             functions: {
                 "lights" : true,
-                "raw": {"0": true}
             }
 
         }
+        expect(actual).toEqual(expected);
     });
 });
--- command-node/z21-udp-helper.js
+++ command-node/z21-udp-helper.js
@@ -1,4 +1,6 @@
-﻿    function prependCount(buf) {
+﻿    const LAN_X = 0x40;
+    const LOCO_INFO = 0xEF;
+    function prependCount(buf) {
         let newBuffer = Buffer.from([buf.length + 2, 0x00]);
         let dgram = Buffer.concat([newBuffer, buf]);
         return dgram;
@@ -12,7 +14,43 @@
         return output;
     };
 
+    function getType(buffer) {
+        if (buffer[2]=== LAN_X && buffer[4]===LOCO_INFO ) {
+            return "loco";            
+        } else {
+            return "unkown";
+        }
+    }
+    function getLocoInfo(buffer, parsed) {
+        parsed.address = buffer[6];
+        parsed.direction = (buffer[8] & 0b10000000) > 0 ? "forward" : "backwards";
+        parsed.speed = buffer[8] & 0b01111111;
+        let steps = buffer[7] & 0b00000111;
+        if (steps = 0b100) {
+            parsed.speedSteps = 128;
+         } else if (steps= 0b10) {
+             parsed.speedSteps = 28;
+         } else {
+             parsed.speedSteps = 14;
+         }
+         parsed.functions = {
+             lights : (buffer[9] & 0b00010000) > 0
+         };
+         
+        
+    }
+    function parsePackage(buffer) {
+        let parsed =  {}    ;
+        parsed.type = getType(buffer);
+        if (parsed.type === "loco") {
+            getLocoInfo(buffer, parsed);
+        }
+        return parsed;
+    }
+    
+
 module.exports = {
     prependCount : prependCount,
-    getXorPackage:  getXorPackage
+    getXorPackage:  getXorPackage,
+    parsePackage: parsePackage
 }
